1b6d47bd25cc03cfd8a5ccb7643bc585b9705af4
Matt Pierce <matt@luckynumberthirteen.com>
2026-02-22 14:47:14 -0800

    Initial Commit

---
+++ TravelersWorkoutTracker/TravelersWorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelersWorkoutTrackerApp.swift
+//  TravelersWorkoutTracker
+//
+//  Created by Matt Pierce on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct TravelersWorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelersWorkoutTrackerTests/TravelersWorkoutTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelersWorkoutTrackerTests.swift
+//  TravelersWorkoutTrackerTests
+//
+//  Created by Matt Pierce on 2/22/26.
+//
+
+import Testing
+@testable import TravelersWorkoutTracker
+
+struct TravelersWorkoutTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
